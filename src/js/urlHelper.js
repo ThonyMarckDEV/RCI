84d7e417c2526a343cff72f0b3afdd205edb2bc4
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-01-04 04:57:59 -0500

    domain static

--- src/js/urlHelper.js
+++ src/js/urlHelper.js
@@ -1,4 +1,4 @@
 // src/js/urlHelper.js
-const API_BASE_URL = 'https://dealers-philippines-thomson-circulation.trycloudflare.com'; // Cambia esta URL si es necesario
+const API_BASE_URL = 'https://talararci.thonymarckdev.online'; // Cambia esta URL si es necesario
 
 export default API_BASE_URL
