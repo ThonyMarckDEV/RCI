c64dc02cc7a26d63c45b6c92da6b6c7671fc9561
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-01-04 04:33:48 -0500

    ADDED NEW CORRECTIONS

--- src/ui/superadminUI/EditarUsuario.jsx
+++ src/ui/superadminUI/EditarUsuario.jsx
@@ -7,7 +7,7 @@ function EditarUsuario() {
       <Sidebar />
 
       <div className="flex-1 p-8 bg-gray-100 overflow-auto">
-        <h2 className="text-2xl font-bold mb-4">Lista de Usuarios</h2>
+        <h2 className="text-2xl font-bold mb-4 text-yellow-500">Lista de Usuarios</h2>
         <div className="overflow-x-auto">
           <UsersTable />
         </div>
